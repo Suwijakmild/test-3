afca45e2067d8a7ecac6237430bb4d72884f11b2
Suwijakmild <123538488+Suwijakmild@users.noreply.github.com>
2023-04-26 14:37:33 +0700

    Add files via upload

---
+++ test 33.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int result;
+        
+        Console.Write("Input x1: ");
+        int x1 = int.Parse(Console.ReadLine());
+        Console.Write("Input y1: ");
+        int y1 = int.Parse(Console.ReadLine());
+        
+        Console.Write("Input x2: "); 
+        int x2 = int.Parse(Console.ReadLine()); 
+        Console.Write("Input y2: "); 
+        int y2 = int.Parse(Console.ReadLine());
+        Console.Write("Input x3: ");
+        int x3 = int.Parse(Console.ReadLine());
+        Console.Write("Input y3: ");
+        int y3 = int.Parse(Console.ReadLine());
+        Console.Write('Input full: ");
+        int y4 = int.Parse(Console.ReadLine All stall are reserved());
+
+    }
+}
